5869f840d618da387dc2e05a7c9a440616aa9966
golova-m <golova.m@bk.ru>
2024-10-11 12:50:51 +0400

    mini Refactoring

--- task1/task1.py
+++ task1/task1.py
@@ -5,9 +5,9 @@ def app(n, m):
     print(n, m)
     main_array = [i for i in range(1, n + 1)]
     step = m - 1
-    dif = 1 + step
-    print(1) 
-    while dif != 1:               
+    print(main_array[0]) 
+    dif = main_array[0] + step 
+    while dif != main_array[0]:               
         if dif > n:
             dif -= n
         else:
